47925f119fc17e483358c09e218c4c334713a29a
Bobby Kazimiroff <robert.kazimiroff@gmail.com>
2023-06-04 20:56:57 -0700

    working modal pulling set values out in view submission

--- commands/cmd_serve_task.py
+++ commands/cmd_serve_task.py
@@ -3,22 +3,32 @@ import pprint
 from slack_bolt import App
 import commands.serve_task_modal
 
-def Register_serve_task_command(app):
+serve_task_prefix = ""
+def Register_serve_task_command(app, id_prefix):
+    serve_task_prefix = id_prefix
     @app.command("/serve-task")
     def handle_command(body, ack, respond, client, logger):
         logger.info(body)
         ack()
         res = client.views_open(
         trigger_id=body["trigger_id"],
-        view=commands.serve_task_modal.Init_Modal
+        view=commands.serve_task_modal.get_init_modal(serve_task_prefix)
         )
         logger.info(res)
 
-    @app.view("serve-task-view-id")
+    @app.action(f"{id_prefix}-environment_select")
+    def handle_some_action(ack, body, logger):
+        ack()
+
+    @app.view(f"{id_prefix}-serve-task-view-id")
     def view_submission(ack, body, logger):
         print("IN VIEW_SUBMISSION")
         ack()
-        pprint.pprint(body)
-        logger.info(body["view"]["state"]["values"])
+        response_values = body["view"]["state"]["values"]
+        pprint.pprint(response_values)
+        res_env = response_values['environment_select_block'][f"{id_prefix}-environment_select"]['selected_option']['value']
+        res_id = response_values['item_id_block'][f"{id_prefix}-item_id_label"]['value']
+        res_amt = response_values['item_amt_block'][f"{id_prefix}-item_amt_label"]['value']
+        logger.info(f"{res_env} {res_id} {res_amt}")
     
 
--- commands/serve_task_modal.py
+++ commands/serve_task_modal.py
@@ -1,83 +1,98 @@
-Init_Modal = {
-	"type": "modal",
-	"callback_id": "serve-task-view-id",
-	"title": {
-		"type": "plain_text",
-		"text": "Serve Generic Task",
-	},
-	"submit": {
-		"type": "plain_text",
-		"text": "Submit",
-	},
-	"close": {
-		"type": "plain_text",
-		"text": "Cancel",
-	},
-	"blocks": [
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": "Please input the item ID and how many items to add."
-			}
+def get_init_modal(id_prefix):
+	return {
+		"type": "modal",
+		"callback_id": f"{id_prefix}-serve-task-view-id",
+		"title": {
+			"type": "plain_text",
+			"text": "Serve Generic Task",
 		},
-		{
-			"type": "divider"
+		"submit": {
+			"type": "plain_text",
+			"text": "Submit",
 		},
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": "Server Environment"
+		"close": {
+			"type": "plain_text",
+			"text": "Cancel",
+		},
+		"blocks": [
+			{
+				"type": "section",
+				"text": {
+					"type": "mrkdwn",
+					"text": "Please input the item ID and how many items to add."
+				}
 			},
-			"accessory": {
-				"type": "static_select",
-				"placeholder": {
-					"type": "plain_text",
-					"text": "Choose Environment",
-					"emoji": False
+			{
+				"type": "divider"
+			},
+			{
+				"block_id":"environment_select_block",
+				"type": "section",
+				"text": {
+					"type": "mrkdwn",
+					"text": "Server Environment"
 				},
-				"options": [
-					{
-						"text": {
-							"type": "plain_text",
-							"text": "Dev",
-							"emoji": False
-						},
-						"value": "dev"
+				"accessory": {
+					"type": "static_select",
+					"placeholder": {
+						"type": "plain_text",
+						"text": "Environment",
+						"emoji": False
 					},
-					{
-						"text": {
-							"type": "plain_text",
-							"text": "Stage",
-							"emoji": False
+					"action_id": f"{id_prefix}-environment_select",
+					"initial_option":{
+							"text": {
+								"type": "plain_text",
+								"text": "Dev",
+								"emoji": False
+							},
+							"value": "dev"
 						},
-						"value": "stage"
-					}
-				]
-			}
-		},
-		{
-			"type": "input",
-			"element": {
-				"type": "plain_text_input"
+					"options": [
+						{
+							"text": {
+								"type": "plain_text",
+								"text": "Dev",
+								"emoji": False
+							},
+							"value": "dev"
+						},
+						{
+							"text": {
+								"type": "plain_text",
+								"text": "Stage",
+								"emoji": False
+							},
+							"value": "stage"
+						}
+					]
+				}
 			},
-			"label": {
-				"type": "plain_text",
-				"text": "Item ID",
-				"emoji": False
-			}
-		},
-		{
-			"type": "input",
-			"element": {
-				"type": "plain_text_input"
+			{
+				"block_id":"item_id_block",
+				"type": "input",
+				"element": {
+					"type": "plain_text_input",
+					"action_id": f"{id_prefix}-item_id_label",
+				},
+				"label": {
+					"type": "plain_text",
+					"text": "Item ID",
+					"emoji": False
+				}
 			},
-			"label": {
-				"type": "plain_text",
-				"text": "Amount",
-				"emoji": False
+			{
+				"block_id":"item_amt_block",
+				"type": "input",
+				"element": {
+					"type": "plain_text_input",
+					"action_id": f"{id_prefix}-item_amt_label",
+				},
+				"label": {
+					"type": "plain_text",
+					"text": "Amount",
+					"emoji": False
+				}
 			}
-		}
-	]
-}
+		]
+	}
