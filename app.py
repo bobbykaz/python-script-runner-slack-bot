be5822c9489a6ef5ed360b41df683ffe93943486
Bobby Kazimiroff <robert.kazimiroff@gmail.com>
2023-06-15 20:29:50 -0700

    Remove samples

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import logging
 from queue import Queue
 from threading import Thread
 from slack_bolt import App
-from commands.serve_task.cmd import Register_serve_task_command 
 from health import health_check
 import command_queue
 
@@ -15,14 +14,17 @@ def log_request(logger, body, next):
     logger.debug(body)
     return next()
 
-@app.command("/hello-bolt-python")
-def hello(body, ack):
-    user_id = body["user_id"]
-    ack(f"Hi <@{user_id}>!")
+work_queue = Queue()
 
-commands = Queue()
-Register_serve_task_command(app, commands, "st")
-Thread(target=command_queue.get_bg_work_loop(commands, app, "bots"), daemon=True).start()
+###### Add Commands Below ################
+from commands.serve_task.cmd import Register_serve_task_command 
+Register_serve_task_command(app, work_queue, "st")
+
+from commands.basic_example.cmd import Register_example_command
+Register_example_command(app, work_queue)
+##########################################
+
+Thread(target=command_queue.get_bg_work_loop(work_queue, app, "bots"), daemon=True).start()
 
 from flask import Flask, request
 from slack_bolt.adapter.flask import SlackRequestHandler
